07a77fd94fa61903844b51bddbb5778e714a8986
Lewis Cowles <lewis-cowles@codesign2.co.uk>
2013-09-22 01:23:57 +0100

    Update example.php
    forgot to keep vars in... added now oops...

--- example.php
+++ example.php
@@ -2,15 +2,15 @@
 include './EpiCurl.php';
 $mc = EpiCurl::getInstance();
 
-$mc->addURL('http://www.yahoo.com'); // call yahoo
-$mc->addURL('http://www.google.com'); // call google
-$mc->addURL('http://www.ebay.com'); // call ebay
+$yahoo = $mc->addURL('http://www.yahoo.com'); // call yahoo
+$google = $mc->addURL('http://www.google.com'); // call google
+$ebay = $mc->addURL('http://www.ebay.com'); // call ebay
 
 // fetch response from yahoo and google
 echo "The response code from Yahoo! was {$yahoo->code}\n";
 echo "The response code from Google was {$google->code}\n";
 
-$mc->addURL('http://www.microsoft.com'); // call microsoft
+$microsoft = $mc->addURL('http://www.microsoft.com'); // call microsoft
 
 // fetch response from ebay and microsoft
 echo "The response code from Ebay was {$ebay->code}\n";
