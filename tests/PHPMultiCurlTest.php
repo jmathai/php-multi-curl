2af01d8b00103417327f8571b643ff064b308b5e
unknown <samt@samt.st>
2015-10-22 19:25:12 +0200

    example page into unit test

---
+++ tests/PHPMultiCurlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use JMathai\PhpMultiCurl\EpiCurl;
+
+class BaseTest extends PHPUnit_Framework_TestCase {
+ 
+	public function testExample()
+	{
+		$mc = EpiCurl::getInstance();
+
+		$yahoo = $mc->addURL('http://www.yahoo.com'); // call yahoo
+		$google = $mc->addURL('http://www.google.com'); // call google
+		$ebay = $mc->addURL('http://www.ebay.com'); // call ebay
+		
+		// fetch response from yahoo and google
+		echo "The response code from Yahoo! was {$yahoo->code}\n";
+		echo "The response code from Google was {$google->code}\n";
+		
+		$microsoft = $mc->addURL('http://www.microsoft.com'); // call microsoft
+
+		// fetch response from ebay and microsoft
+		echo "The response code from Ebay was {$ebay->code}\n";
+		echo "The response code from Microsoft was {$microsoft->code}\n";
+	}
+}
