3993398f74ae412f815f54558e186abcce44c5b8
Slawek Kaleta <s.kaleta@citycore.pro>
2018-02-03 18:48:53 +0100

    Update phpunit tests

--- tests/CallsTest.php
+++ tests/CallsTest.php
@@ -1,45 +1,46 @@
-<?php use JMathai\PhpMultiCurl\MultiCurl;
+<?php 
+use JMathai\PhpMultiCurl\MultiCurl;
 
 class CallsTest extends PHPUnit_Framework_TestCase
 {
-  public function testGetCode()
-  {
-    $mc = MultiCurl::getInstance();
-    $google = $mc->addURL('http://www.google.com'); // call google
+    public function testGetCode()
+    {
+        $mc = MultiCurl::getInstance();
+        $google = $mc->addURL('http://www.google.com'); // call google
     
-    $this->assertInternalType('integer', $google->code);
-  }
+        $this->assertInternalType('integer', $google->code);
+    }
 
-  public function testGetCode200()
-  {
-    $mc = MultiCurl::getInstance();
-    $google = $mc->addUrl('http://www.google.com');
+    public function testGetCode200()
+    { 
+       $mc = MultiCurl::getInstance();
+       $github = $mc->addUrl('https://github.com');
     
-    $this->assertEquals(200, $google->code);
-  }
+       $this->assertEquals(200, $github->code);
+    }
 
-  public function testGetCode404()
-  {
-    $mc = MultiCurl::getInstance();
-    $google = $mc->addUrl('http://www.example.com/404');
+    public function testGetCode404()
+    {
+        $mc = MultiCurl::getInstance();
+        $google = $mc->addUrl('https://www.google.com/404');
     
-    $this->assertEquals(404, $google->code);
-  }
+        $this->assertEquals(404, $google->code);
+    }
 
-  public function testResponseJson()
-  {
-    $mc = MultiCurl::getInstance();
-    $call = $mc->addUrl('http://jsonplaceholder.typicode.com/users');
-    $response = json_decode($call->response, true);
-    $this->assertInternalType('array', $response);
-  }
+    public function testResponseJson()
+    {
+        $mc = MultiCurl::getInstance();
+        $call = $mc->addUrl('http://jsonplaceholder.typicode.com/users');
+        $response = json_decode($call->response, true);
+        $this->assertInternalType('array', $response);
+    }
 
-  public function testResponseHeaders()
-  {
-    $mc = MultiCurl::getInstance();
-    $google = $mc->addUrl('http://www.example.com/404');
+    public function testResponseHeaders()
+    {
+        $mc = MultiCurl::getInstance();
+        $google = $mc->addUrl('https://www.google.com');
     
-    $this->assertInternalType('array', $google->headers);
-    $this->assertNotEmpty($google->headers['Etag']);
-  }
+        $this->assertInternalType('array', $google->headers);
+        $this->assertNotEmpty($google->headers['Date']);
+    }
 }
